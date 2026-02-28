5c12df13d5ce4ade0cac37a051e69835e41c257e
jayaram-java <jayaramp51096@gmail.com>
2026-02-28 17:14:48 +0530

    Eureka client

--- customer-management-service/src/main/java/com/bank/customermanagement/customer_management_service/CustomerManagementServiceApplication.java
+++ customer-management-service/src/main/java/com/bank/customermanagement/customer_management_service/CustomerManagementServiceApplication.java
@@ -2,10 +2,12 @@ package com.bank.customermanagement.customer_management_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication // @Configuraton , @EnableAutoconfiguration, @ComponentScan
 @EnableScheduling
+@EnableDiscoveryClient
 public class CustomerManagementServiceApplication {
 
 	public static void main(String[] args) {
